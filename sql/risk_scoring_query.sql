3a2793ff1f7ba8ec0dc366cc66847a5f92d7ad6e
Tori Seymour <toriseymour83@gmail.com>
2026-05-17 17:55:32 +0100

    Update risk_scoring_query.sql

--- sql/risk_scoring_query.sql
+++ sql/risk_scoring_query.sql
@@ -1,5 +1,11 @@
 USE LearnerReengagementTracker;
 GO
+
+DROP VIEW IF EXISTS vw_learner_risk_scores;
+GO
+
+CREATE VIEW vw_learner_risk_scores AS
+
 WITH scored_learners AS (
 
     SELECT
@@ -7,9 +13,7 @@ WITH scored_learners AS (
         l.learner_name,
         l.provision_type,
         l.tutor,
-
         a.attendance_rate,
-
         e.last_work_submitted_days,
         e.last_contact_days,
         e.recent_positive_contact,
@@ -22,38 +26,29 @@ WITH scored_learners AS (
                 WHEN a.attendance_rate < 80 THEN 1
                 ELSE 0
             END
-
             +
-
             CASE
                 WHEN e.last_work_submitted_days > 20 THEN 3
                 WHEN e.last_work_submitted_days > 14 THEN 2
                 WHEN e.last_work_submitted_days > 7 THEN 1
                 ELSE 0
             END
-
             +
-
             CASE
                 WHEN e.last_contact_days > 14 THEN 2
                 WHEN e.last_contact_days > 7 THEN 1
                 ELSE 0
             END
-
             +
-
             CASE
                 WHEN e.previous_disengagement = 'Yes' THEN 2
                 ELSE 0
             END
-
             +
-
             CASE
                 WHEN e.recent_positive_contact = 'No' THEN 1
                 ELSE 0
             END
-
         ) AS total_risk_score
 
     FROM learners_sample l
@@ -74,3 +69,7 @@ SELECT
     END AS risk_level
 
 FROM scored_learners;
+GO
+
+SELECT *
+FROM vw_learner_risk_scores;
